081ef352241dae0c3aabc8bfccdec8524db25a41
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-24 17:15:08 -0300

    Adding interfaces for login

---
+++ app/src/main/java/com/sibela/presentationlayermvp/LoginTask.kt
@@ -0,0 +1,18 @@
+package com.sibela.presentationlayermvp
+
+interface LoginTask {
+
+    interface View {
+        fun displayLoading()
+
+        fun displayUserNotFound()
+
+        fun displayInvalidUserOrPassword()
+
+        fun onLoggedIn()
+    }
+
+    interface Presenter {
+        fun loginUser(email: String, password: String)
+    }
+}
